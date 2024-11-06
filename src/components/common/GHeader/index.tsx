196c0fc91c8d6c80a84cabe1710426cf624a58fd
hiimjayson <hiimjaysondev@gmail.com>
2024-11-06 17:07:24 +0900

    style: update GHeader height

--- src/components/common/GHeader/index.tsx
+++ src/components/common/GHeader/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 export function GHeader({ onInputClick }: Props) {
   return (
-    <header className="w-full h-12 px-4 py-2 bg-white border-b border-gray-200">
+    <header className="w-full h-16 px-4 py-3 bg-white border-b border-gray-200">
       {onInputClick != null ? (
         <div
           className="flex items-center gap-2 w-full sm:w-[400px] h-full px-3 rounded-full bg-gray-100 border border-gray-300 text-gray-500 cursor-pointer"
